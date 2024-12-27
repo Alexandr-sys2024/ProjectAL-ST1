0c737ead6613e0c91ec303ace4b040f79194b997
Alexandr-sys2024 <time2016slot@gmail.com>
2024-12-27 12:22:46 +0300

    add new code in stack

--- stack.py
+++ stack.py
@@ -1,48 +1,28 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+class Stack:
+   def __init__(self):
+       self.items = []
+
+   def is_empty(self):
+       return self.items == []
+
+   def push(self, item):
+       self.items.append(item)
+
+   def pop(self):
+       return self.items.pop()
+
+   def peek(self):
+       return self.items[-1]
 
 
-class Graph:
-    def __init__(self):
-        self.graph = {}
+stack = Stack()
 
-    def add_edge(self, u, v):
-        # Добавление ребра в граф
-        if u not in self.graph:
-            self.graph[u] = []
-        self.graph[u].append(v)
+print(stack.is_empty())
 
-    def print_graph(self):
-        # Вывод смежностей для каждой вершины
-        for node in self.graph:
-            print(node, "->", " -> ".join(map(str, self.graph[node])))
+stack.push(1)
+stack.push(2)
+stack.push(3)
 
-    def draw_graph(self):
-        # Визуализация графа с использованием NetworkX
-        G = nx.DiGraph()  # Направленный граф
-        for node in self.graph:
-            for neighbor in self.graph[node]:
-                G.add_edge(node, neighbor)
+print(stack.is_empty())
 
-        # Отображение графа
-        nx.draw(G, with_labels=True, node_size=2000, node_color="skyblue", font_size=12)
-        plt.show()
-
-
-# Пример использования:
-g = Graph()
-
-# Добавление рёбер
-g.add_edge(0, 1)
-g.add_edge(0, 4)
-g.add_edge(1, 2)
-g.add_edge(1, 3)
-g.add_edge(1, 4)
-g.add_edge(2, 3)
-g.add_edge(3, 4)
-
-# Печать графа
-g.print_graph()
-
-# Визуализация графа
-g.draw_graph()
+print(stack.peek())
